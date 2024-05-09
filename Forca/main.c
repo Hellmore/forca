ea2b02dadba03b773389615ca1ce55f5afea06f4
Heloisa <heloisa@fatec.campinas.br>
2024-05-09 15:43:09 -0300

    Vitoria/derrota

--- Forca/main.c
+++ Forca/main.c
@@ -10,12 +10,16 @@ int inicia_jogo();
 void desenha_forca(int contador, int inicio);
 int verifica_tamanho(char* palavra);
 char* cria_string_vazia(char* string_vazia, int tamanho);
-void verifica_tentativa(char* palavra, char* palavra_vazia, char* erros, int contador_erros);
+void verifica_tentativa(char* palavra, char* palavra_vazia, char* erros, int* contador_erros);
+int verifica_vitoria(char* palavra_vazia);
+void mostra_acertos(char* palavra_vazia);
 void mostra_erros(char erros[], int contador_erros);
+void mostra_correta(char* palavra);
+void vitoria();
+void derrota();
 
 
-int main()
-{
+int main(){
     int inicio_do_jogo, contador_erros = 0, tamanho_palavra;
     char palavra[100], palavra_vazia[100], erros[6] = {' '};
 
@@ -31,12 +35,31 @@ int main()
             strcpy(palavra_vazia, cria_string_vazia(palavra_vazia, tamanho_palavra));
             do {
                 desenha_forca(contador_erros, 0);
-                verifica_tentativa(palavra, palavra_vazia, erros, contador_erros);
+                verifica_tentativa(palavra, palavra_vazia, erros, &contador_erros);
+                system("cls");
+                mostra_acertos(palavra_vazia);
                 mostra_erros(erros, contador_erros);
-            } while (contador_erros <= 6);
 
+                if (verifica_vitoria(palavra_vazia) == 1) {
+                    system("cls");
+                    vitoria();
+                    desenha_forca(contador_erros, 1);
+                    break;
+                }
 
+                if (contador_erros >= 6) {
+                    system("cls");
+                    derrota();
+                    mostra_correta(palavra);
+                    desenha_forca(contador_erros, 1);
+                    break;
+                }
 
+                if (strcmp(palavra, palavra_vazia) != 0 && contador_erros < 6) {
+                    continue;
+                }
+
+            } while (1);
         }
 
     } else {
@@ -60,8 +83,6 @@ int menu_principal(){
     printf("\n#        #     ####  #  #  ####  #  #          #         #");
     printf("\n#                                            #   #       #");
     printf("\n#                                          #       #     #");
-    printf("\n#                                                        #");
-    printf("\n#                                                        #");
     printf("\n#                                                        #");
     printf("\n#        Bem vindo ao jogo da forca!                     #");
     printf("\n#                                                        #");
@@ -181,36 +202,43 @@ void desenha_forca(int contador, int inicio){
                 case 0:
                     printf("Você tem 6 tentativas!\n");
                     printf("_____________   \n|.|         |   \n|.|        _|_\n|.|          \n|.|        \ \n|.|         \n|.| \n|.| \n|.| \n\n");
+                    printf("\n");
                     break;
 
                 case 1:
                     printf("Você tem 5 tentativas!\n");
                     printf("_____________   \n|.|         |   \n|.|        _|_\n|.|         O    \n|.|         \n|.|       \n|.| \n|.| \n|.| \n\n");
+                    printf("\n");
                     break;
 
                 case 2:
                     printf("Você tem 4 tentativas!\n");
                     printf("_____________   \n|.|         |   \n|.|        _|_\n|.|         O    \n|.|         |    \n|.|         \n|.| \n|.| \n|.| \n\n");
+                    printf("\n");
                     break;
 
                 case 3:
                     printf("Você tem 3 tentativas!\n");
                     printf("_____________   \n|.|         |   \n|.|        _|_\n|.|         O    \n|.|        /|    \n|.|         \n|.| \n|.| \n|.| \n\n");
+                    printf("\n");
                     break;
 
                 case 4:
                     printf("Você tem 2 tentativas!\n");
                     printf("_____________   \n|.|         |   \n|.|        _|_\n|.|         O    \n|.|        /|\\    \n|.|         \n|.| \n|.| \n|.| \n\n");
+                    printf("\n");
                     break;
 
                 case 5:
                     printf("Você tem apenas 1 tentativa!\n");
                     printf("_____________   \n|.|         |   \n|.|        _|_\n|.|         O    \n|.|        /|\\    \n|.|        /  \n|.|  \n|.|  \n|.| \n\n");
+                    printf("\n");
                     break;
 
                 case 6:
                     printf("FORCA!!!\n");
                     printf("_____________   \n|.|         |   \n|.|        _|_\n|.|         O    \n|.|        /|\\    \n|.|        / \\ \n|.|  \n|.|  \n|.| \n\n");
+                    printf("\n");
                     break;
             }
 
@@ -236,27 +264,40 @@ char* cria_string_vazia(char* string_vazia, int tamanho){
     return string_vazia;
 }
 
-void verifica_tentativa(char* palavra, char* palavra_vazia, char* erros, int contador_erros){
+void verifica_tentativa(char* palavra, char* palavra_vazia, char* erros, int* contador_erros) {
     char chute;
-    int acertos = 0;
-    int erros_cometidos = 0;
+    int acerto = 0;
     printf("Digite a letra que deseja chutar:\n");
+    scanf(" %c", &chute);
     getchar();
-    scanf("%c", &chute);
-    getchar();
-    for (int i = 0; palavra[i] != '\n' && palavra[i] != '\0'; i++){
-        if (chute == palavra[i]){
+
+    for (int i = 0; palavra[i] != '\0'; i++) {
+        if (chute == palavra[i]) {
             palavra_vazia[i] = chute;
-            acertos++;
+            acerto = 1;
         }
-    }
-    if (acertos == 0) {
-        if (contador_erros < 6){
-            erros[contador_erros] = chute;
-        }
-        contador_erros++;
     }
 
+    if (!acerto && *contador_erros < 6) {
+        erros[*contador_erros] = chute;
+        (*contador_erros)++;
+    }
+}
+
+int verifica_vitoria(char* palavra_vazia){
+    int todas_letras_adivinhadas = 1;
+    for (int i = 0; palavra_vazia[i] != '\0'; i++){
+        if (palavra_vazia[i] == '_'){
+            todas_letras_adivinhadas = 0;
+            break;
+        }
+    }
+    return todas_letras_adivinhadas;
+}
+
+void mostra_acertos(char* palavra_vazia){
+    printf("Letras acertadas: \n");
+    printf("\n%s\n\n", palavra_vazia);
 }
 
 void mostra_erros(char erros[], int contador_erros){
@@ -265,9 +306,81 @@ void mostra_erros(char erros[], int contador_erros){
         for (int i = 0; i < contador_erros; i++){
         printf("%c\n", erros[i]);
         }
+        printf("\n");
     } else if (contador_erros == 0) {
-        printf("Você não errou... ainda!\n");
+        printf("Você não errou... ainda!\n\n");
     }
 }
 
+void mostra_correta(char* palavra) {
+    printf("\n\nA palavra correta era...\n");
+    printf("\n%s\n\n", palavra);
+}
 
+void vitoria() {
+    printf("\n##########################################################");
+    printf("\n#                                                        #");
+    printf("\n#                                                        #");
+    printf("\n#    ##    ##  ##  ######  ######  ######  ##   ####     #");
+    printf("\n#    ##    ##  ##    ##    ##  ##  ##  ##  ##  ##  ##    #");
+    printf("\n#    ##    ##  ##    ##    ##  ##  ##  ##  ##  ##  ##    #");
+    printf("\n#    ##    ##  ##    ##    ##  ##  ######  ##  ######    #");
+    printf("\n#    ##    ##  ##    ##    ##  ##  ###     ##  ##  ##    #");
+    printf("\n#    ##    ##  ##    ##    ##  ##  ## #    ##  ##  ##    #");
+    printf("\n#     ##  ##   ##    ##    ##  ##  ##  #   ##  ##  ##    #");
+    printf("\n#       ##     ##    ##    ######  ##   #  ##  ##  ##    #");
+    printf("\n#                                                        #");
+    printf("\n#                                                        #");
+    printf("\n#                        *                               #");
+    printf("\n#      *                                  *      *       #");
+    printf("\n#             *                    *                     #");
+    printf("\n#                          ###                           #");
+    printf("\n#                   *      # #                 *         #");
+    printf("\n#      *       *           ###         *                 #");
+    printf("\n#                       #   #   #                        #");
+    printf("\n#                         # # #                          #");
+    printf("\n#          *                #             *              #");
+    printf("\n#                           #                   *        #");
+    printf("\n#                           #                            #");
+    printf("\n#              *            #                            #");
+    printf("\n#     *                    # #        *          *       #");
+    printf("\n#                         #   #                          #");
+    printf("\n#                                                        #");
+    printf("\n#                                                        #");
+    printf("\n#                                                        #");
+    printf("\n##########################################################\n");
+}
+
+void derrota(){
+    printf("\n###########################################################");
+    printf("\n#                                                         #");
+    printf("\n#                                                         #");
+    printf("\n#  ####    ######  ######  ######  ######  ######  ####   #");
+    printf("\n#  ##  ##  ##      ##  ##  ##  ##  ##  ##    ##   ##  ##  #");
+    printf("\n#  ##  ##  ##      ##  ##  ##  ##  ##  ##    ##   ##  ##  #");
+    printf("\n#  ##  ##  ####    ######  ######  ##  ##    ##   ######  #");
+    printf("\n#  ##  ##  ##      ###     ###     ##  ##    ##   ##  ##  #");
+    printf("\n#  ##  ##  ##      ## #    ## #    ##  ##    ##   ##  ##  #");
+    printf("\n#  ##  ##  ##      ##  #   ##  #   ##  ##    ##   ##  ##  #");
+    printf("\n#  ####    ######  ##   #  ##   #  ######    ##   ##  ##  #");
+    printf("\n#                                                         #");
+    printf("\n#                                                         #");
+    printf("\n#                                                         #");
+    printf("\n#                                                         #");
+    printf("\n#                                                         #");
+    printf("\n#                          ###                            #");
+    printf("\n#                          # #                            #");
+    printf("\n#                          ###                            #");
+    printf("\n#                           #                             #");
+    printf("\n#                         # # #                           #");
+    printf("\n#                        #  #  #                          #");
+    printf("\n#                           #                             #");
+    printf("\n#                           #                             #");
+    printf("\n#                           #                             #");
+    printf("\n#                          # #                            #");
+    printf("\n#                         #   #                           #");
+    printf("\n#                                                         #");
+    printf("\n#                                                         #");
+    printf("\n#                                                         #");
+    printf("\n###########################################################\n");
+}
